179c91ebd2f18b8c695f912370aebf403c810089
Xaltio <gregoire.vottero@gmail.com>
2021-10-29 19:27:19 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Xavier_Scripts/Colliders.cs
+++ Assets/Scripts/Xavier_Scripts/Colliders.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 using UnityEngine.UI;
+
 public class Colliders : MonoBehaviour
 {
-
-
     public bool CanPress;
 
     public int index;
-    public float Score;
+    public float ScoreValue;
+    Text score;
 
    
     public Animator anim;
@@ -20,30 +19,28 @@ public class Colliders : MonoBehaviour
 
     private void Start()
     {
-        Score = 0;
+        ScoreValue = 0;
+        score = GetComponent<Text>();
     }
+
+    private void Update()
+    {
+        score.text = "Score: " + ScoreValue;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Arrows"))
         {
-            
-
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                Score += 5;
+                ScoreValue += 5;
             }
-
-          
-
         }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
         
     }
-
-   
-
-
 }
 
